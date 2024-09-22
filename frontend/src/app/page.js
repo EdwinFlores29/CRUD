31a71cfc9fb5a7eb2610bcd46750ce3c3e150fbd
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-09-21 21:13:50 -0600

    formulario tarea
    sobre el archivo para el frontend --> FormTask.jsx

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,9 +3,9 @@ import ListTask from "@/app/components/ListTask";
 
 function HomePage (){
   return (
-      <div>
-        <h1>Task App</h1>
-          <div>
+      <div className="container p-7">
+        <h1 className="font-bold">Task App</h1>
+          <div className="flex gap-x-10">
               <FormTask />
               <ListTask />
           </div>
